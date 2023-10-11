f84ddc6b767cd04253d1d633b862ebd334b2bb47
Emmanuel-uzoezie <mac@macs-MacBook-Pro.local>
2023-10-11 02:48:30 +0100

    hopefully the last push

--- pages/index.tsx
+++ pages/index.tsx
@@ -120,16 +120,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-     <div>
+     <div className={inter.className}>
         <div className='h-screen max-w-[500px] mx-auto'>
-          {/* {isUserLogin ?
+          {isUserLogin ?
               <div>
                 <HomeComponent />
               </div>
               :
               <SignUpComponent />
-            } */}
-            <HomeComponent />
+            }
+            {/* <HomeComponent /> */}
         </div>
      </div>
     </>
